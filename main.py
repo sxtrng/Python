09acae87c43b2a0171e6e19b1bff2a62ead9483a
sxtrng <sxtrng@gmail.com>
2025-08-22 14:55:34 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-class Account:
-    def __init__(self, initial):
-        self.balance = initial
+if __name__ == '__main__':
 
-    def display_balance(self):
-        print('Balance', self.balance)
+    print('First Python Program')
 
-    def __add__(self, other):
-        total = self.balance + other.balance
-        return Account(total)
-
-
-account_one = Account(50)
-account_two = Account(10)
-
-new_account = account_one + account_two
-new_account.display_balance()
+    name = 'Mike'
+    print(name)
