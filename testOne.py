f48176ca30ca18a11da861e6be90ffb559f55213
sxtrng <sxtrng@gmail.com>
2025-08-22 19:50:01 +0000

    added user input and type annotaations

--- testOne.py
+++ testOne.py
@@ -1,3 +1,14 @@
 name = 'mike'
 age = 39
 print(f'{name} is {age} years old.')
+
+userName: str
+userAge: int
+
+userName = input('enter your name: ')
+
+print(f'Hello {userName}')
+
+userAge = int(input('Enter your age: '))
+
+print(f'{userName}, you look good for {userAge}.')
